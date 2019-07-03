875a6f30ea179d64884435e09135a546af0ae482
Keshav Tadimeti <keshavtadimeti@gmail.com>
2019-07-03 15:26:42 -0700

    Navbar functionality largely working, sticky scroll

--- debuggedlabs-site/src/app/navigation/navigation-bar/navigation-bar.component.ts
+++ debuggedlabs-site/src/app/navigation/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, Injectable, HostListener } from '@angular/core';
+import { trigger, state, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
-  styleUrls: ['./navigation-bar.component.css']
+  styleUrls: ['./navigation-bar.component.css'],
+  animations: [
+    trigger('fade',
+      [
+        state('void', style({ opacity: 0 })),
+        transition(':enter', [animate(300)]),
+        transition(':leave', [animate(500)]),
+      ]
+    )]
 })
+
+@Injectable({
+  providedIn: 'root'
+})
+
 export class NavigationBarComponent implements OnInit {
+
+  public show: boolean = false;
+  public heightCutOff: number = 400;
 
   constructor() { }
 
   ngOnInit() {
+    this.heightCutOff = document.getElementById('banner').offsetHeight * 2;
+    console.log("Banner is this tall: ", this.heightCutOff);
+  }
+
+  @HostListener('window:scroll') onScroll() {
+    console.log("Being called!")
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > this.heightCutOff) {
+      this.show = true;
+
+    }
+    else if (window.pageYOffset <= this.heightCutOff) {
+      this.show = false;
+    }
   }
 
 }
--- debuggedlabs-site/src/app/sections/frontpage/frontpage.component.ts
+++ debuggedlabs-site/src/app/sections/frontpage/frontpage.component.ts
@@ -5,6 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './frontpage.component.html',
   styleUrls: ['./frontpage.component.css']
 })
+
 export class FrontpageComponent implements OnInit {
 
   constructor() { }
