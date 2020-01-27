1e25c10dcdc2578dc0bd871be424822d9c584124
Keshav Tadimeti <keshavtadimeti@gmail.com>
2020-01-26 17:00:08 -0800

    Making About page responsive to width

--- debuggedlabs-site/src/app/config/profiles-config.ts
+++ debuggedlabs-site/src/app/config/profiles-config.ts
@@ -15,8 +15,8 @@ export const TeamProfileData: TeamProfile[] = [
     {
         name: 'Shawn Barman',
         position: "Founder, Managing Editor",
-        bioShort: "Bio short goes here",
-        bio: "Bio goes here",
+        bioShort: "A legit scientist who loves PB&J.",
+        bio: "Shawn Barman is a research associate at The Scripps Research Institute in San Diego and a published researcher for his discovery of a bacteriophage called Phalcolm. He studied microbiology, immunology, and molecular genetics at UCLA. Barman spent his college years being an ardent scientist, a personalized film critic, and a victorious gamer. He is also a passionate defender of the nutritious value of a PB&J and the superior taste of Taco Bell.",
         imageUrl: "assets/profiles/shawn.jpg",
         twitterHandle: "",
         email: "sbarman@debuggedlabs.com",
--- debuggedlabs-site/src/app/sections/about/about-post/about-post.component.ts
+++ debuggedlabs-site/src/app/sections/about/about-post/about-post.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, HostListener } from '@angular/core';
 import { TeamProfile } from 'src/app/definitions/teamProfile';
 
 @Component({
@@ -9,10 +9,25 @@ import { TeamProfile } from 'src/app/definitions/teamProfile';
 export class AboutPostComponent implements OnInit {
 
   @Input() profile: TeamProfile;
+  private windowWidth: number;
+  private widthCutOff: number = 952;
 
+  @HostListener('window:resize', ['$event']) onResize(event) {
+    this.windowWidth = window.innerWidth;
+  }
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  getImageInlineClass() 
+  {
+    return this.windowWidth <= this.widthCutOff ? 'image-block': 'image-inline';
+  }
+
+  showAltTitle(): boolean{
+    return this.windowWidth <= this.widthCutOff;
   }
 
 }
