31cfc647a9412418fe60d79d427b889c211146f3
Keshav Tadimeti <keshavtadimeti@gmail.com>
2020-06-20 04:14:06 -0700

    Adding footer and copyright symbol

---
+++ src/app/navigation/footer/footer.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { PageDetailsService, PageId } from 'src/app/services/page-details.service';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor(private pageDetailService: PageDetailsService) { }
+
+  ngOnInit() {
+  }
+
+  /**
+   * Get subheader class name based on the current page we're on
+   */
+  getSubheaderClassName(): string {
+    var className = "footer-subhead";
+    const pageId = this.pageDetailService.getCurrentPageId();
+    if (pageId === PageId.Podcasts) {
+      return className + "-podcast";
+    }
+    else if (pageId === PageId.Technology) {
+      return className + "-technology";
+    }
+    else if (pageId === PageId.About) {
+      return className + "-about";
+    }
+    return className;
+  }
+
+  /**
+   * Get current year
+   */
+  getCurrentYear(): number {
+    return (new Date()).getFullYear();
+  }
+
+}
