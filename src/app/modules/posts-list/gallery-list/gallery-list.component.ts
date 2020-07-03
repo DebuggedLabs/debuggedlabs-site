e61b3ea6ccc05e559047f01b20885a40bb53d3fd
Keshav Tadimeti <keshavtadimeti@gmail.com>
2020-07-03 00:42:53 -0700

    Adding gallery posts

--- src/app/modules/posts-list/gallery-list/gallery-list.component.ts
+++ src/app/modules/posts-list/gallery-list/gallery-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostsListBaseComponent } from '../posts-list-base/posts-list-base.component';
 import { WidthService } from 'src/app/services/width.service';
+import { Post } from 'src/app/definitions/interfaces';
 
 @Component({
   selector: 'app-gallery-list',
@@ -9,11 +10,15 @@ import { WidthService } from 'src/app/services/width.service';
 })
 export class GalleryListComponent extends PostsListBaseComponent implements OnInit {
 
+  public primaryPost: Post;
+  public secondaryPost: Post;
+
   constructor(private myWidthService: WidthService) {
     super(myWidthService);
   }
 
   ngOnInit() {
+    this.primaryPost = this.posts[0];
+    this.secondaryPost = this.posts[1];
   }
-
 }
