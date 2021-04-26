81620cd7d0d20a4172b37a86a399a105355d0f6d
Keshav Tadimeti <keshavtadimeti@gmail.com>
2021-04-25 20:32:09 -0700

    Adding button in pages to link back to home page (#20)
    * Reduce size of Keshav profile
    
    * Adding link back to home page

---
+++ src/app/modules/home-page-button/home-page-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-page-button',
+  templateUrl: './home-page-button.component.html',
+  styleUrls: ['./home-page-button.component.css']
+})
+export class HomePageButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
